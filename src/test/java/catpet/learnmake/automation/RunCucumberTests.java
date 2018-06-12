6e463ea01b05e3b9db00098ece9bde6940e1eb98
Yilmaz Guleryuz <yilmaz@guleryuz.net>
2018-06-12 20:36:45 +0200

    base impl

---
+++ src/test/java/catpet/learnmake/automation/RunCucumberTests.java
@@ -0,0 +1,13 @@
+package catpet.learnmake.automation;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber", "junit:target/cucumber.xml"},
+        features = "src/test/resources/catpet/learnmake/automation",
+        glue = {"catpet.learnmake.automation.stepdefs"})
+public class RunCucumberTests {
+}
